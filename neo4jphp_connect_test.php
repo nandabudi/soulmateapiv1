67ad49ff3467fab42533187b97c6c770548c8f3d
Nanda <nbprayuga@digdaya.com>
2015-08-12 23:55:03 +0700

    add image upload handler

--- neo4jphp_connect_test.php
+++ neo4jphp_connect_test.php
@@ -2,4 +2,6 @@
     require('vendor/autoload.php');
 
     $client = new Everyman\Neo4j\Client('localhost', 7474);
+    $client->getTransport()
+      ->setAuth('neo4j', 'soulmate');
     print_r($client->getServerInfo());
