0602a6d903fa25e8964145479b7be709eb4f594c
Nanda <nbprayuga@digdaya.com>
2015-08-13 00:35:23 +0700

    add upload handler

--- app/Http/Controllers/DonaturController.php
+++ app/Http/Controllers/DonaturController.php
@@ -16,7 +16,7 @@ class DonaturController extends Controller{
   public $_userNeo4j = 'neo4j';
   public $_passNeo4j = 'soulmate';
   public $_label = 'Donatur';
-  public $_uriImage = 'http://soulmateapi.cloudapp.net/api/v1/images/';
+  public $_uriImage = 'http://localhost:8000/api/v1/images/';
 
   public function loginDonatur(Request $request){
     $client = new Client($this->_host, $this->_port);
